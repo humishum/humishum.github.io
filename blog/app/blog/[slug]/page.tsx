92c9e89fac77cc86faf2dc722d0137434163acf4
Humdaan Mustafa <humishum@gmail.com>
2025-02-15 00:08:57 -0800

    dependenciessss  '

--- blog/app/blog/[slug]/page.tsx
+++ blog/app/blog/[slug]/page.tsx
@@ -11,7 +11,8 @@ export async function generateStaticParams() {
   }))
 }
 
-export function generateMetadata({ params }) {
+export async function generateMetadata({ params }) {
+  params = await params;
   let post = getBlogPosts().find((post) => post.slug === params.slug)
   if (!post) {
     return
@@ -51,7 +52,8 @@ export function generateMetadata({ params }) {
   }
 }
 
-export default function Blog({ params }) {
+export default async function Blog({ params }) {
+  params = await params;
   let post = getBlogPosts().find((post) => post.slug === params.slug)
 
   if (!post) {
