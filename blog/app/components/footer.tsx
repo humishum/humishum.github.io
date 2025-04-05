efd84ee985877c4a34dae998be5c70b92c08a4e6
Humdaan Mustafa <humishum@gmail.com>
2025-04-05 14:52:31 -0700

    fotter

--- blog/app/components/footer.tsx
+++ blog/app/components/footer.tsx
@@ -30,6 +30,17 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/humishum"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">X fka Twitter</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} humdaan
